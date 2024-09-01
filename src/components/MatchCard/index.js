4d8b73cd097fb098d45fdafc43c3b8273b56ba49
ManojKumar2301 <manojkumarpendekanti23@gmail.com>
2024-09-01 06:06:23 +0000

    solution

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,30 @@
+// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {matchCardData} = props
+  const {competingTeamLogo, competingTeam, result, matchStatus} = matchCardData
+
+  const isMatchStatusWon = matchStatus === 'Won'
+
+  return (
+    <li className="match-card-bg-container">
+      <img
+        className="match-card-competing-team-logo"
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+      />
+      <p className="match-card-competing-team-name">{competingTeam}</p>
+      <p className="match-card-result">{result}</p>
+      <p
+        className={`match-card-status ${
+          isMatchStatusWon ? 'game-won' : 'game-lost'
+        }`}
+      >
+        {matchStatus}
+      </p>
+    </li>
+  )
+}
+
+export default MatchCard
